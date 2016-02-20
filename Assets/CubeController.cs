db019d5c86d2185c6120a21720da5d16e498c06a
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2016-02-21 03:52:04 +0900

    Colorful cube falls.

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject CubePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnCube());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnCube()
+    {
+        while (true)
+        {
+            var cube = Instantiate<GameObject>(CubePrefab);
+            cube.transform.position = new Vector3(Random.Range(-5, 5) + 0.5f, 10, Random.Range(-5, 5) + 0.5f);
+            var meshRenderer = cube.GetComponent<MeshRenderer>();
+            var material = meshRenderer.material;
+            material.color = new Color(Random.value, Random.value, Random.value);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
