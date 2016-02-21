2abf30c106ec17eb59bc84f53053abc8609d9502
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2016-02-22 01:59:36 +0900

    The floor tilts by the key input.

---
+++ Assets/FloorController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var z = Input.GetAxis("Horizontal");
+        var x = Input.GetAxis("Vertical");
+        var rot = transform.rotation;
+        rot.x = rot.x + (x / 4 - rot.x) / 2;
+        rot.z = rot.z + (-z / 4 - rot.z) / 2;
+        transform.rotation = rot;
+    }
+}
